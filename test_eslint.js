454aa8629c19bb88cfe9ce69df4b3178c48bb0cf
Thedyingdutchman <71300765+Thedyingdutchman@users.noreply.github.com>
2020-12-07 22:19:29 -0500

    trying to commit

---
+++ test_eslint.js
@@ -0,0 +1,11 @@
+let didShowMessage = false;
+
+function ShowMessage() {
+    const message = 'I like dogs';
+
+    for (let i = 0; i < 5; i++) {
+        console.log(message);
+    }
+}
+
+ShowMessage();
